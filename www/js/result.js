588c8dec29d1c972e853de5f5989654c76bc89f4
Anas <mahdisomjee04@gmail.com>
2024-10-16 12:08:27 +0530

    Added NFC

--- www/js/result.js
+++ www/js/result.js
@@ -1,5 +1,9 @@
 var dictionary = null;
+var nfcResult = null;
 var accura;
+var DOB = "";
+var DOE = "";
+var PassNo = "";
 
 const app = {
     init:()=>{
@@ -13,6 +17,8 @@ const app = {
         document.querySelector('.ready').addEventListener('click', app.back);
         document.querySelector('#faceMatch').addEventListener('click', app.facematch);
         document.querySelector('#liveness').addEventListener('click', app.liveness);
+
+        app.createPopup();
 
         var urlParams = new URLSearchParams(window.location.search);
 
@@ -64,7 +70,13 @@ const app = {
             
             app.displayImage(dict.front_img,"frontImage",false)
         }
-
+        if(dict.face != null){
+            var faceButton = document.getElementById("faceMatch"); 
+            var liveButton = document.getElementById("liveness");
+            faceButton.style.visibility = "visible"
+            liveButton.style.visibility = "visible"
+           app.displayImage(dict.face,"faceImage",true)
+        }
         if(dict.back_img != null){
             var imgElement = document.getElementById("BackImg");
             imgElement.textContent = "Back Image";
@@ -76,6 +88,10 @@ const app = {
 
             app.displayImage(dict.back_img,"backImage",false);
         }
+
+        document.getElementById("okButton").onclick = function () {
+            app.handlePopupOk();
+        };
     },
 
     frontTable:()=>{
@@ -249,7 +265,13 @@ const app = {
         for (let i = 0; i < keys.length; i++) {
         const key = keys[i];
         const value = values[i];
-        
+        if (key == "Document No.") {
+            PassNo = value;
+        }else if(key == "Date of Birth"){
+            DOB = value;
+        }else if(key == "Date of Expiry"){
+            DOE = value;
+        }
         const row = document.createElement("tr");
         const keyCell = document.createElement("td");
         const valueCell = document.createElement("td");
@@ -261,9 +283,44 @@ const app = {
         row.appendChild(valueCell);
         tbody.appendChild(row);
         }
-        
+        app.showPopup();
         // Append the table to a container element in the HTML
         const container = document.getElementById("mrztable");
+        container.appendChild(table);
+
+
+    },
+    nfcTable:()=>{
+        // Retrieve the keys and values from the dictionary
+        const keys = Object.keys(nfcResult);
+        const values = Object.values(nfcResult);
+
+        // Create the HTML structure for the table
+        const table = document.createElement("table");
+        const caption = document.createElement("caption");
+        caption.textContent = "NFC"; // Set the heading text
+        table.appendChild(caption);
+        const tbody = document.createElement("tbody");
+        table.appendChild(tbody);
+        
+        // Generate the table rows dynamically using the keys and values
+        for (let i = 0; i < keys.length; i++) {
+        const key = keys[i];
+        const value = values[i];
+
+        const row = document.createElement("tr");
+        const keyCell = document.createElement("td");
+        const valueCell = document.createElement("td");
+        
+        keyCell.textContent = key;
+        valueCell.textContent = value;
+        
+        row.appendChild(keyCell);
+        row.appendChild(valueCell);
+        tbody.appendChild(row);
+        }
+        // Append the table to a container element in the HTML
+        const container = document.getElementById("nfctable");
         container.appendChild(table);
 
 
@@ -272,6 +329,7 @@ const app = {
     back:()=>{
         history.back();
     },
+    
 
     facematch:()=>{
         var accuraConfs = {
@@ -341,7 +399,7 @@ const app = {
             setBlurPercentage: 80,
             setGlarePercentage_0: -1,
             setGlarePercentage_1: -1,
-            setLivenessURL: 'https://accurascan.com:8443/check_liveness',
+            setLivenessURL: '',
             setFeedBackLowLightMessage: 'Low light detected',
             feedbackLowLightTolerence: 39,
             feedbackDialogMessage: 'Loading...',
@@ -419,7 +477,151 @@ const app = {
             });
         }
 
-    }
+    },
+
+    createPopup: () => {
+        // Create popup HTML and inject into body
+        const popupHTML = `
+        <div id="popup" class="popup">
+            <div class="popup-content">
+                <span class="close">&times;</span>
+                <h2 class="start_NFC">Press OK to start NFC</h2>
+                <label class="start_NFC" for="passportNumber">Passport Number:</label><br>
+                <input type="text" id="passportNumber" name="passportNumber"><br><br>
+                <label class="start_NFC" for="dob">DOB:</label><br>
+                <input type="date" id="dob" name="dob"><br><br>
+                <label class="start_NFC" for="doe">DOE:</label><br>
+                <input type="date" id="doe" name="doe"><br><br>
+                <button type="button" id="okButton">OK</button>
+            </div>
+        </div>
+        `;
+
+        document.body.insertAdjacentHTML('beforeend', popupHTML);
+
+        // Add popup styles
+        const style = document.createElement('style');
+        style.innerHTML = `
+        .start_NFC {
+            color: black;
+        }
+        .popup {
+            display: none;
+            position: fixed;
+            z-index: 1;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            overflow: auto;
+            background-color: rgba(0,0,0,0.4);
+        }
+        .popup-content {
+            background-color: #fefefe;
+            margin: 15% auto;
+            padding: 20px;
+            border: 1px solid #888;
+            width: 80%;
+            max-width: 300px;
+            border-radius: 10px;
+        }
+        .close {
+            color: #aaa;
+            float: right;
+            font-size: 28px;
+            font-weight: bold;
+        }
+        .close:hover,
+        .close:focus {
+            color: black;
+            text-decoration: none;
+            cursor: pointer;
+        }
+        `;
+        document.head.appendChild(style);
+
+        // Add event listener for close button
+        document.querySelector('.close').onclick = function () {
+            document.getElementById("popup").style.display = "none";
+        };
+
+        // Close popup when clicking outside of it
+        window.onclick = function (event) {
+            if (event.target == document.getElementById("popup")) {
+                document.getElementById("popup").style.display = "none";
+            }
+        };
+
+        // Add event listener for the OK button
+        document.getElementById('okButton').onclick = app.handlePopupOk;
+    },
+
+    showPopup: () => {
+        var popup = document.getElementById("popup");
+        
+        function convertDateFormat(inputDate) {
+            // Split the input date by hyphen
+            const [day, month, year] = inputDate.split('-');
+        
+            // Determine the century
+            let fullYear;
+            if (year.length === 2) {
+                fullYear = parseInt(year) < 50 ? '20' + year : '19' + year;
+            } else {
+                fullYear = year;
+            }
+        
+            // Create a new Date object
+            const date = new Date(fullYear, month - 1, day);
+        
+            // Get the formatted date parts
+            const formattedYear = date.getFullYear();
+            const formattedMonth = ('0' + (date.getMonth() + 1)).slice(-2); // Ensure two digits
+            const formattedDay = ('0' + date.getDate()).slice(-2); // Ensure two digits
+        
+            // Return the formatted date
+            return `${formattedYear}-${formattedMonth}-${formattedDay}`;
+        }
+        
+        
+        // Prefill the popup with values
+        var passportNumber = document.getElementById("passportNumber");
+        var dob = document.getElementById("dob");
+        var doe = document.getElementById("doe");
+
+        console.log("passportNumber:", PassNo);
+        console.log("dob:", DOB);
+        console.log("DOE:", DOE);
+
+        // Replace with actual data
+        passportNumber.value = PassNo || '';
+        dob.value = convertDateFormat(DOB) || '';
+        doe.value = convertDateFormat(DOE) || '';
+
+        popup.style.display = "block";
+    },
+    
+
+    handlePopupOk: () => {
+        const passportNumber = document.getElementById("passportNumber").value;
+        const dob = document.getElementById("dob").value;
+        const doe = document.getElementById("doe").value;
+
+        console.log("Passport Number:", passportNumber);
+        console.log("DOB:", dob);
+        console.log("DOE:", doe);
+
+        
+        accura.startNFC(passportNumber,dob,doe,
+            function success(result){
+                nfcResult = result
+                app.nfcTable();
+                console.log(result)
+           },function(error){
+            alert(error);
+           })
+        document.getElementById("popup").style.display = "none";
+    },
 
 }
 
